c11aba8d15fcb9b565cb666ebb0f2af00af8155e
eagskunst <emmanuelnypd@gmail.com>
2018-11-20 22:28:26 -0400

    Añadido animaciones y validaciones

--- src/ui/Tablero.java
+++ src/ui/Tablero.java
@@ -1,14 +1,10 @@
 package ui;
 
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
-import javax.swing.Timer;
 
 import listeners.BoxMouseListener;
 
@@ -47,28 +43,11 @@ public class Tablero extends JPanel{
 			}
 			boxes[i].changeBorder(false);
 		}
-		BoxMouseListener boxListener = new BoxMouseListener(boxes);
+		BoxMouseListener boxListener = new BoxMouseListener(boxes,panel);
 		for(int i = 0;i<boxes.length;i++) {
 			boxes[i].addMouseListener(boxListener);
 		    panel.add(boxes[i]);
 		}
-		
-		/*Timer t = new Timer(200, new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(count<8) {
-					System.out.println("entered");
-					Dimension d1 = boxes[5].getPreferredSize();
-					boxes[5].setBounds(boxes[5].getX(), boxes[5].getY()+5, d1.width, d1.height);
-					count++;
-					Dimension d = boxes[0].getPreferredSize();
-					boxes[0].setBounds(boxes[0].getX(), boxes[0].getY()+5, d.width, d.height);
-					
-				}
-			}
-		});
-		t.start();*/
 		add(panel);
 	}
 	
--- src/util/CustomTimer.java
+++ src/util/CustomTimer.java
@@ -1,15 +1,70 @@
 package util;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
+import javax.swing.JPanel;
 import javax.swing.Timer;
 
+import ui.BoxImage;
+
 public class CustomTimer extends Timer{
-	private int repetition;
+	private int repetition = 0;
+	private int col;
+	private Random rand = new Random();
+	public static boolean isAnimating = false;
+
 	public CustomTimer(int delay, ActionListener listener) {
 		super(delay, listener);
-		
+	}
+	public int getRepetition() {
+		return repetition;
+	}
+	public void setRepetition(int repetition) {
+		this.repetition = repetition;
+	}
+	public void executeTransition(int j, BoxImage boxes[], JPanel panel,BoxImage lastBox) {
+		isAnimating = true;
+		boolean isLastBox = false;
+		this.col = j;
+		boxes[j].setVisible(false);
+		boxes[j].setAdding(false);
+		boxes[j].changeBorder(false);
+		if(repetition<8) {
+			int d = j-5;
+			while(d>=0) {
+				Dimension d1 = boxes[d].getPreferredSize();
+				boxes[d].setBounds(boxes[d].getX(), boxes[d].getY()+5, d1.width, d1.height);
+				d-=5;
+			}
+			repetition++;
+		}
+		else {
+			for(int k = j;k>=0;k-=5) {
+				if(boxes[k] == lastBox && !boxes[k].isAdding()) {
+					System.out.println("entered is lastbox");
+					isLastBox = true;
+					col = lastBox.getNumber();
+				}
+				boxes[k].setAdding(false);
+				boxes[k].changeBorder(false);
+
+				if(k-5>=0)
+					boxes[k].setNumber(boxes[k-5].getNumber());
+				else {
+					int r = rand.nextInt(3);
+					boxes[k].setNumber(r == 1 ? 2:4);
+				}
+			}
+			if(isLastBox)
+				boxes[j].setNumber(col);
+			boxes[j].setVisible(true);
+			panel.repaint();
+			isAnimating = false;
+			this.stop();
+		}
 	}
 	
-
 }
